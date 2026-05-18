a176f55df4caba1b85a00a428fbabcaf7623e56c
Code Bloded <tetisimiak@gmail.com>
2026-05-18 02:05:59 +0100

    patch

--- src/app/api/school/principal/route.ts
+++ src/app/api/school/principal/route.ts
@@ -14,7 +14,7 @@ export async function POST(req: Request) {
     }
 
     const school = await prisma.school.findFirst({
-      where: { clerkOrgId: orgId || '' },
+      where: { clerkOrgId: userId || '' },
     })
 
     if (!school) {
