a84eeac8a44057d36cea55ea2aa1d7b47af287f5
Code Bloded <tetisimiak@gmail.com>
2026-06-14 22:56:41 +0200

    deploy

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  serverExternalPackages: ['@sparticuz/chromium', 'puppeteer-core'],
   /* config options here */
-};
+}
 
 export default nextConfig;
